b1c67a16ee697d1c1f9d7a1c20b83ee5404cac5d
banlonjones <banlonjones@go-groups.net>
2022-05-25 19:43:17 +0100

    linter error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || categories_path
   end
+
+  def after_sign_up_path_for(resource)
+    stored_location_for(resource) || categories_path
+  end
 end
